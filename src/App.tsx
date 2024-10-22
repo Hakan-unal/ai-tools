145b4de072c0ee05f86d49cabb66ce5807828876
Con Sınov <hakanunal_1996@hotmail.com>
2024-10-23 01:31:07 +0300

    updated

--- src/App.tsx
+++ src/App.tsx
@@ -1,66 +1,101 @@
-import { Row, Typography, Col, Image, Switch } from "antd";
-import {
-   title1,
-   title2,
-   title3,
-   paragraph1,
-   paragraph2,
-   paragraph3,
-   paragraph4,
-   paragraph5,
-   paragraph6,
-} from "./language/language";
-import { useState } from "react";
+import { Row, Typography, Col } from "antd";
 
 const { Title, Paragraph } = Typography;
 
-const App = () => {
-   const [translate, setTranslate] = useState<boolean>(false);
+const App = () => (
+  <Row justify={"center"} gutter={[24, 36]}>
+    <Col span={20}>
+      <Title style={{ textAlign: "center" }} level={1}>
+        Yapay Zeka (İstatistik)
+      </Title>
 
-   return (
-      <Row justify={"center"}>
-         <Switch
-            unCheckedChildren={<div>UK</div>}
-            checkedChildren={<div>EN</div>}
-            onChange={(checked) => setTranslate(checked)}
-         />
+      <Paragraph>
+        Geçmiş verilerden yola çıkarak mevcut durum hakkında oluşabilecek en
+        uygun seçeneği bulma noktasında insana benzeyen bir yapı. Data öğrenme
+        noktasını ve büyük veriyi insanın doğumundan itibaren başlayıp kendi
+        bilinçaltı, tecrübesi ve karar verme mekanizması gibi düşünürsek çok
+        fazla benzerlik mevcut. İnsan bunu tek bir cihazla tüm farklı senaryolar
+        için gerçekleştirebilirken. Şuan için yapay zeka farklı farklı dallarda
+        (görüntü, ses, sağlık, duyuşsal vb.) birbirinden ayrı şekilde
+        yapabiliyor. Burada anlatılmak istenen durumu biraz daha açıklamak
+        gerekirse insan doğumundan itibaren başlayıp yaşadığı olaylara verdiği
+        tepkiler ve tecrübesiyle karşılaşacağı yeni durumlar için çözüm üretip
+        hamle yapabiliyor bunu yaparken çevresinde de faydalanıyor. Şuan
+        gelişmekte olan çeşitli ai ürünleri de kendisinden beklenen durumla
+        ilgili öğrenmesi gereken datayı işleyip karşılaşacağı yeni durumlarla
+        ilgili en uygun seçimi yapmayı hedefliyor ve yeni durumu çözüp bunu da
+        kendisine tecrübe olarak ekliyor. Peki bir konuyla ilgili bilgiye sahip
+        olmak veya fikir belirtmek onu mutlak doğru yapar mı sorusunun cevabı
+        çoğunlukla hayır olacağından mevcut akışta en uygun seçeneği tercih edip
+        yola devam etmek gerekli.
+      </Paragraph>
 
-         <Col span={24}>
-            <Title level={1}>{title1(translate)}</Title>
+      <Title style={{ textAlign: "center" }} level={3}>
+        ChatGpt - Google
+      </Title>
 
-            <Paragraph>{paragraph1(translate)}</Paragraph>
-         </Col>
-         <Col sm={4}>
-            <Image
-               width={"100%"}
-               src="https://as1.ftcdn.net/v2/jpg/03/43/21/92/1000_F_343219263_kBIXF8URy0yD4K5voCm2fAxA3ri8xrbY.jpg"
-            />
-         </Col>
-         <Col span={24}>
-            <Title level={2}>{title2(translate)}</Title>
-            <Paragraph>{paragraph2(translate)}</Paragraph>
-         </Col>
+      <Paragraph>
+        Chatgpt'ye soru sorup cevap aldığınızda yarattığı hissiyat açısından
+        sanki karşınızda biri var ve size ne yapmanızı söylemiş gibi bir izlenim
+        yarattığından bir kaç sene öncesinde yer alan google'lı yıllar çabuk
+        unutuluyor. Burada farkı yaratan başlangıç dürtüsü; bir konuyu google'da
+        araştırmaya başlarsanız ve birbirinden farklı 4 sayfayı inceleyip
+        kendinize uygun gelebilecek cevabı bulup çıkış yaparsanız bunu
+        araştırmış olursunuz. Ancak chatgpt veya farklı bir tool ile sizin
+        sorduğunuz soruyu yine aynı 4 sayfayı inceleyip tamamı olmasa da bir
+        parça özetini önünüzde bulursanız yine size uygun gelebilecek cevabı
+        bulup çıkış yaparsanız ama bunu geçmişe nazaran daha hızlı şekilde
+        gerçekleştirirsiniz. Günümüzde chatGpt ile hiçbir şey yapmadan kod
+        yazılabildiğini düşünen bir çok insan var bu düşünce karşınızda size
+        cevap veren birisi var efekti yarattığından eğer tüm github repoları
+        derlenip sorduğum soru doğrultusunda istatistiki sonuçları önümde
+        göreceksem ortalamanın ortalaması vasattır ve başlangıçta ne aradığınızı
+        bilmiyorsanız ne bulduğunuzun önemi kalmaz. Burada farklı bir bakış
+        açısıyla baktığımızda dünyanın süper gücü amerika başkanının yürüme ve
+        konuşma zorluğu çektiği emeklilik yaşının çoktan gelipte geçtiğini fark
+        edemeyen insanlarla aynı koda veya fikre ortak olmak istemezsiniz.
+        Burada ortalamanın ortalamasının vasat olmasından kasıt budur ve vasata
+        tahammül edebilmek zordur. Keşke kendisiyle çengelköyde çorba içebilsek
+        "afiyet olsun" derdim.
+      </Paragraph>
 
-         <Col sm={4}>
-            <Image
-               width={"100%"}
-               src="https://img.freepik.com/premium-vector/vector-blackboard-with-tasks-sorting-scheme-kanban-board-template-blank-stickers-work_566075-71.jpg"
-            />
-         </Col>
+      <Title style={{ textAlign: "center" }} level={3}>
+        Yapay Zeka (İstatistik) - İş
+      </Title>
 
-         <Col span={24}>
-            <Title level={3}>{title3(translate)}</Title>
-            <Paragraph>
-               <ul>
-                  <li>{paragraph3(translate)}</li>
-                  <li>{paragraph4(translate)}</li>
-                  <li>{paragraph5(translate)}</li>
-                  <li>{paragraph6(translate)}</li>
-               </ul>
-            </Paragraph>
-         </Col>
-      </Row>
-   );
-};
+      <Paragraph>
+        Geçmişe bakıp mevcut yaptığınız işte insani yorum yapmanız gerekmiyorsa
+        evet alır. Ancak bu yeni iş süreçleri doğurur yani sonuçta değişen bir
+        şey olmaz para kazanıp harmayacan bir şey sizin yerinize geçmez hiçbir
+        devlet dünya ekonomisine reset atmak istemez istese de atamaz
+        lidyalılardan önceki takas dönemine dönmek istemiyorlarsa. Burada yeni
+        ürünler açtıkları yolda geride boşluk bıraktıkları ve yeni boşlukları
+        dolduran yeni süreçler getirdiğinden tek bir yönde değil çeşitli
+        alanlarda bilgi sahibi olmak her zaman iş bulma konusunda işe yarar.
+        Sanayi devriminden öncede her şey yolundaydı sonrasında da değişen bir
+        şey olmadı fabrikalar otomasyon süreçlerine geçip daha farklı iş kolları
+        açıldığında da değişen bir şey olmadı
+      </Paragraph>
+
+      <Title style={{ textAlign: "center" }} level={3}>
+        Yapay Zeka (İstatistik) - İnsan
+      </Title>
+
+      <Paragraph>
+        Şuan gelişme aşamasında ve birbirinden ayrı kollarda olan yapay zeka
+        ürünleri bir gün birleşip insanı oluşturabilir mi ? Bunu öğrenmesi
+        gerekeceği kesin peki ama bunu öğrenecek olsa nasıl olurdu sorusunun
+        cevabını vermeye çalışalım ? Üniversite yıllarında fark edilen bir
+        gencin kendisinden habersiz şekilde bir çip yerleştirsek ve tüm algı
+        fikir yorum karar mekanizmalarınızı yaşayışını takip etsek bunu
+        öğretebiliriz. Tabi bu uzun macerada habersiz girdikleri delikte 62'den
+        yapılmayan tavşanla karşılaşan insanlar var mıdır bilinmez? Çünkü insan
+        başlı başına bir bilinmez ve bazen pes etmek üzere olduğunuzda veya
+        nasıl yapacağınızı bilmediğinizde birisi çıkar ve size aramadığınız
+        şeylerin cevabını bile verebilir.
+      </Paragraph>
+    </Col>
+  </Row>
+);
 
 export default App;
